ecedd7b2291ae851658a556eb3e23e98709969e6
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-03-18 15:42:14 +0700

    pertama

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul4_1302204126
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
